d5e21570cc3b787ddf8dbf3a24c342757fcdd4ce
Taistelubanaani <munkkinen92@gmail.com>
2015-01-24 13:57:15 +0200

    DIOO

---
+++ Assets/PlayerOne.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOne : MonoBehaviour 
+{
+	public AudioClip huuto1;
+	public AudioClip huuto2;
+	public AudioClip huuto3;
+	public AudioClip huuto4;
+	public AudioClip huuto5;
+	public AudioClip huuto6;
+	public AudioClip huuto7;
+	public AudioClip huuto8;
+	public AudioClip huuto9;
+	public AudioClip huuto10;
+	public AudioClip huuto11;
+
+	[HideInInspector]
+	public Sprite defaultPose;
+	[HideInInspector]
+	public RuntimeAnimatorController animations;
+}
