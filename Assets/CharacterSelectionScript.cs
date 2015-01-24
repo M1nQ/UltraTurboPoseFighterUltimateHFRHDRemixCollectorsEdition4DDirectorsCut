69f119770724af443763475d20e1c36aa8cee089
Jusauh <jusasassi@gmail.com>
2015-01-24 18:49:54 +0200

    sdfsdf
    fsdfsdf

---
+++ Assets/CharacterSelectionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectionScript : MonoBehaviour {
+    public bool twoPlayer;
+    CursorScript p1Selection = GameObject.Find("Cursor p1").GetComponent<CursorScript>();
+    CursorScript p2Selection = GameObject.Find("Cursor p2").GetComponent<CursorScript>();
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	    if(p1Selection.isSelecting == false && p2Selection.isSelecting == false)
+        {
+
+        }
+	}
+}
